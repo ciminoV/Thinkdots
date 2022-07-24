6ca7b9f4469068500966eb4cd67257d44cf7c18b
cimino <ciminovincenzo1@gmail.com>
2022-07-24 14:44:27 +0200

    add nvim to .config
    set options, keymaps, plugins, colorscheme and autocommands

---
+++ .config/nvim/lua/user/autocommands.lua
@@ -0,0 +1,13 @@
+-- Fixes Autocomment
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  callback = function()
+    vim.cmd "set formatoptions-=cro"
+  end,
+})
+
+-- Highlight Yanked Text
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+  callback = function()
+    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+  end,
+})
