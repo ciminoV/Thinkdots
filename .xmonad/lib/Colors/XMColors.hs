122759bb4790ee753acbd271bf921a2e60b8085b
ciminoV <ciminovincenzo1@gmail.com>
2022-08-06 16:34:09 +0200

    add colors

---
+++ .xmonad/lib/Colors/XMColors.hs
@@ -0,0 +1,23 @@
+module Colors.XMColors where
+
+import XMonad
+
+colorBack = "#2E3440"
+colorFore = "#BBC2CF"
+
+white    = "#ffffff"
+black    = "#101010"
+red0     = "#f07178"
+red1     = "#ff8b92"
+green0   = "#c3e88d"
+green1   = "#ddffa7"
+yellow0  = "#ffcb6b"
+yellow1  = "#ffe585"
+blue0    = "#82aaff"
+blue1    = "#9cc4ff"
+violet0  = "#c792ea"
+violet1  = "#e1acff"
+azure0   = "#89ddff"
+azure1   = "#a3f7ff"
+gray0    = "#d0d0d0"
+gray1    = "#434758"
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,11 +1,20 @@
 ------------------------------------------------------------------------
 -- IMPORTS
 ------------------------------------------------------------------------
+
+-- Base
 import XMonad
 import System.IO (hClose, hPutStr)
 import System.Exit (exitSuccess)
 import qualified XMonad.StackSet as W
 
+-- Actions
+import XMonad.Actions.CopyWindow (kill1)
+
+-- Data
+import Data.Char (toUpper)
+
+-- Hooks
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
@@ -13,10 +22,7 @@ import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 import XMonad.Hooks.EwmhDesktops
 
-import XMonad.Actions.CopyWindow (kill1)
-
-import Data.Char (toUpper)
-
+-- Util
 import XMonad.Util.EZConfig
 import XMonad.Util.NamedActions
 import XMonad.Util.Loggers
@@ -24,13 +30,18 @@ import XMonad.Util.ClickableWorkspaces
 import XMonad.Util.Ungrab
 import XMonad.Util.Run (spawnPipe)
 
+-- Layout
 import XMonad.Layout.Magnifier
 import XMonad.Layout.ThreeColumns
+
+-- Colors
+import Colors.XMColors
 
 ------------------------------------------------------------------------
 -- MAIN
 ------------------------------------------------------------------------
 
+-- Main and all the combinators
 main :: IO ()
 main = xmonad
      . ewmhFullscreen
@@ -39,21 +50,34 @@ main = xmonad
      . addDescrKeys' ((mod4Mask, xK_F1), showKeybindings) myKeys
      $ myConfig
 
--- XMonad configuration
+-- Main configuration
 myConfig = def
     { modMask    = mod4Mask      -- Rebind Mod to the Super key
     , layoutHook = myLayout      -- Use custom layouts
     , manageHook = myManageHook  -- Match on certain windows
     , workspaces = myWorkspaces
     , terminal   = myTerminal
+    , borderWidth        = myBorderWidth
+    , normalBorderColor  = myNormColor
+    , focusedBorderColor = myFocusColor
     }
 
--- Workspaces
+-- Main variables
 myWorkspaces  = [" dev ", " www ", " doc ", " fm ", " mlab ", " var "]
 
--- Useful variables
+myFont = "xft:SauceCodePro Nerd Font Mono:regular:size=9:antialias=true:hinting=true"
+
 myTerminal    = "st"
-myBrowser = "brave"
+
+myBrowser     = "brave"
+
+myEditor = myTerminal ++ " -e nvim "
+
+myBorderWidth = 1           -- Sets border width for windows
+
+myNormColor   = colorBack   -- Border color of normal windows
+
+myFocusColor  = azure0      -- Border color of focused windows
 
 ------------------------------------------------------------------------
 -- XMOBAR
@@ -61,25 +85,24 @@ myBrowser = "brave"
 
 myXmobarPP :: PP
 myXmobarPP = def
-    { ppSep             = magenta " • "
+    { ppSep             = myppSeparator " • "
     , ppTitleSanitize   = xmobarStrip
-    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
-    , ppHidden          = white . wrap " " ""
-    , ppHiddenNoWindows = lowWhite . wrap " " ""
-    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+    , ppCurrent         = myppCurrent . wrap " " "" . xmobarBorder "Bottom" azure0 2
+    , ppHidden          = myppHidden . wrap " " ""
+    , ppHiddenNoWindows = myppHiddenNoWindows . wrap " " ""
+    , ppUrgent          = myppUrgent . wrap "!" "!"
     , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
     , ppExtras          = [lTitle]
     }
   where
-    lTitle = shortenL 50 logTitle
+    lTitle = shortenL 50 (logTitle .| logConst " ")
 
-    blue, lowWhite, magenta, red, white, yellow :: String -> String
-    magenta  = xmobarColor "#ff79c6" ""
-    blue     = xmobarColor "#bd93f9" ""
-    white    = xmobarColor "#f8f8f2" ""
-    yellow   = xmobarColor "#f1fa8c" ""
-    red      = xmobarColor "#ff5555" ""
-    lowWhite = xmobarColor "#bbbbbb" ""
+    myppCurrent, myppSeparator, myppUrgent, myppHidden, myppHiddenNoWindows :: String -> String
+    myppCurrent         = xmobarColor azure0 ""
+    myppSeparator       = xmobarColor violet0 ""
+    myppUrgent          = xmobarColor red0 ""
+    myppHidden          = xmobarColor green0 ""
+    myppHiddenNoWindows = xmobarColor colorFore ""
 
 ------------------------------------------------------------------------
 -- HOOKS
@@ -107,12 +130,14 @@ myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
 -- KEYBINDINGS
 ------------------------------------------------------------------------
 
+-- Subtitle format
 subtitle' ::  String -> ((KeyMask, KeySym), NamedAction)
 subtitle' x = ((0,0), NamedAction $ map toUpper
                       $ sep ++ "\n-- " ++ x ++ " --\n" ++ sep)
   where
     sep = replicate (6 + length x) '-'
 
+-- Pipe the Named Actions output to yad
 showKeybindings :: [((KeyMask, KeySym), NamedAction)] -> NamedAction
 showKeybindings x = addName "Show Keybindings" $ io $ do
   h <- spawnPipe $ "yad --text-info --fontname=\"Hack\" --fore=#46d9ff back=#282c36 --center --geometry=1200x800 --title \"XMonad keybindings\""
@@ -120,16 +145,20 @@ showKeybindings x = addName "Show Keybindings" $ io $ do
   hClose h
   return ()
 
+-- My keybindings
 myKeys :: XConfig l0 -> [((KeyMask, KeySym), NamedAction)]
 myKeys c =
     let subKeys str ks = subtitle' str : mkNamedKeymap c ks in
+    -- Basics
     subKeys "Xmonad Essentials"
     [ ("M-r", addName "Recompile XMonad"         $ spawn "xmonad --recompile")
     , ("M-S-r", addName "Restart XMonad"         $ spawn "xmonad --restart")
     , ("M-S-q", addName "Quit XMonad"            $ io exitSuccess)
     , ("M-q", addName "Kill focused window"      $ kill1)
-    , ("M-<Return>", addName "Run prompt"      $ spawn "st")]
+    , ("M-w", addName "Run browser"              $ spawn myBrowser)
+    , ("M-<Return>", addName "Run prompt"        $ spawn "st")]
 
+    -- Workspaces
     ^++^ subKeys "Switch to workspace"
     [ ("M-1", addName "Switch to workspace 1"    $ (windows $ W.greedyView $ myWorkspaces !! 0))
     , ("M-2", addName "Switch to workspace 2"    $ (windows $ W.greedyView $ myWorkspaces !! 1))
