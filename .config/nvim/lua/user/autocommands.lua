713b1f6b270c69f47510fd348e6e952887005dfa
ciminoV <ciminovincenzo1@gmail.com>
2022-08-18 00:06:46 +0200

    fix comments option

--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -1,10 +1,3 @@
--- Fix autocomment
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-  callback = function()
-    vim.cmd "set formatoptions-=cro"
-  end,
-})
-
 -- Highlight Yanked Text
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   callback = function()
