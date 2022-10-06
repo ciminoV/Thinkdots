45bcbe3cc2b868e59e17c5dc79b7f958f71e731b
ciminoV <ciminovincenzo1@gmail.com>
2022-10-06 13:01:31 +0200

    add figure and aligned equation snippets

--- .config/nvim/lua/user/snippets/texsnips/normal_bwA.lua
+++ .config/nvim/lua/user/snippets/texsnips/normal_bwA.lua
@@ -4,37 +4,61 @@ local t = ls.text_node
 local i = ls.insert_node
 
 local bwA = {
-  s(
-    { trig = "ali", name = "Align" },
-    { t({ "\\begin{align*}", "\t" }), i(1), t({ "", ".\\end{align*}" }) }
-  ),
+	s({ trig = "ali", name = "Align" }, { t({ "\\begin{align*}", "\t" }), i(1), t({ "", ".\\end{align*}" }) }),
 
-  ls.parser.parse_snippet(
-    { trig = "beg", name = "begin{} / end{}" },
-    "\\begin{$1}\n\t$0\n\\end{$1}"
-  ),
+	ls.parser.parse_snippet({ trig = "beg", name = "begin{} / end{}" }, "\\begin{$1}\n\t$0\n\\end{$1}"),
 
-  ls.parser.parse_snippet({ trig = "case", name = "cases" }, "\\begin{cases}\n\t$1\n\\end{cases}"),
+	ls.parser.parse_snippet({ trig = "case", name = "cases" }, "\\begin{cases}\n\t$1\n\\end{cases}"),
 
-  s({ trig = "bigfun", name = "Big function" }, {
-    t({ "\\begin{align*}", "\t" }),
-    i(1),
-    t(":"),
-    t(" "),
-    i(2),
-    t("&\\longrightarrow "),
-    i(3),
-    t({ " \\", "\t" }),
-    i(4),
-    t("&\\longmapsto "),
-    i(1),
-    t("("),
-    i(4),
-    t(")"),
-    t(" = "),
-    i(0),
-    t({ "", ".\\end{align*}" }),
-  }),
+	s({ trig = "bigfun", name = "Big function" }, {
+		t({ "\\begin{align*}", "\t" }),
+		i(1),
+		t(":"),
+		t(" "),
+		i(2),
+		t("&\\longrightarrow "),
+		i(3),
+		t({ " \\", "\t" }),
+		i(4),
+		t("&\\longmapsto "),
+		i(1),
+		t("("),
+		i(4),
+		t(")"),
+		t(" = "),
+		i(0),
+		t({ "", ".\\end{align*}" }),
+	}),
+
+	s("figh", {
+		t({ "\\begin{figure}[h]", "" }),
+		t({ "\\begin{center}", "" }),
+		t("\\includegraphics[width=0.95\\textwidth]{"),
+		i(1),
+		t({ "}", "" }),
+		t({ "\\end{center} ", "" }),
+		t("\\caption{"),
+		i(2),
+		t({ "}", "" }),
+		t("\\label{fig:"),
+		i(0),
+		t({ "}", "" }),
+		t("\\end{figure}"),
+	}),
+
+	s("eqa", {
+		t({ "\\begin{equation}", "\t" }),
+		t({ "\\begin{split}", "\t" }),
+		i(2, "x(t)"),
+		t(" &= "),
+		i(0),
+		t({ " \\\\", "\t"}),
+		t({ "\\end{split} ", "\t" }),
+		t("\\label{eq:"),
+		i(1),
+		t({ "}", "" }),
+		t("\\end{equation}"),
+	}),
 }
 
 return bwA
