b41d247f5e88a96a043c4444a512a7b5f081446e
ciminoV <ciminovincenzo1@gmail.com>
2022-10-06 11:15:55 +0200

    update latex template

--- .config/nvim/lua/user/snippets/texsnips/normal_b.lua
+++ .config/nvim/lua/user/snippets/texsnips/normal_b.lua
@@ -2,31 +2,45 @@ local ls = require("luasnip")
 
 local s = ls.snippet
 local i = ls.insert_node
-local fmt = require("luasnip.extras.fmt").fmt
+local t = ls.text_node
 
 local b = {
 
-  ls.parser.parse_snippet(
-    { trig = "pac", name = "Package" },
-    "\\usepackage[${1:options}]{${2:package}}$0"
-  ),
+	ls.parser.parse_snippet({ trig = "pac", name = "Package" }, "\\usepackage[${1:options}]{${2:package}}$0"),
 
-
-  s("template",
-    fmt(
-        [[
-          \documentclass[a4paper]{{article}}
-
-          \usepackage[utf8]{{inputenc}}
-          \usepackage{{amsmath, amssymb}}
-          
-          \begin{{document}}
-            {} 
-          \end{{document}}
-        ]],
-        {i(1)}
-    )
-  ),
+	s("template", {
+		t({ "\\documentclass[a4paper]{article}", "" }),
+		t({ "", "" }),
+		t({ "\\usepackage[utf8]{inputenc}", "" }),
+		t({ "\\usepackage{amsmath, amssymb}", "" }),
+		t({ "\\usepackage{imakeidx}", "" }),
+		t({ "\\usepackage{graphicx}", "" }),
+		t({ "\\graphicspath{ {./images/} }", "" }),
+		t({ "", "" }),
+		t("\\title{"),
+		i(1),
+		t({ "}", "" }),
+		t("\\author{"),
+		i(2, "Vincenzo Cimino"),
+		t({ "}", "" }),
+		t("\\date{"),
+		i(3, os.date("%Y")),
+		t({ "}", "" }),
+		t({ "", "" }),
+		t({ "\\begin{document}", "" }),
+		t({ "\\begin{titlepage}", "" }),
+		t({ "\\maketitle", "" }),
+		t({ "\\end{titlepage}", "" }),
+		t({ "", "" }),
+		t({ "\\tableofcontents", "" }),
+		t({ "", "" }),
+		t({ "\\clearpage", "" }),
+		t({ "", "" }),
+		i(0),
+		t({ "", "" }),
+		t({ "", "" }),
+		t({ "\\end{document}", "" }),
+	}),
 }
 
 return b
