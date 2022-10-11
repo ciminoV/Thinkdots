6c7a5fbc1e4d9fa221ba87fd18f12e295f9c4a39
ciminoV <ciminovincenzo1@gmail.com>
2022-10-11 21:14:35 +0200

    removed deprecated stuff

--- .config/nvim/lua/user/lsp/handlers.lua
+++ .config/nvim/lua/user/lsp/handlers.lua
@@ -68,10 +68,6 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-  if client.name == "sumneko_lua" then
-    client.resolved_capabilities.document_formatting = false
-  end
-
   lsp_keymaps(bufnr)
 
   local status_ok, illuminate = pcall(require, "illuminate")
