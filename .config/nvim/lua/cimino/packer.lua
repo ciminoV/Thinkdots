6284cdbab1a8faf9180ac375f10cc65e0da58e3c
ciminoV <ciminovincenzo1@gmail.com>
2023-02-27 17:33:13 +0100

    feat nvim: packer.lua refactor

--- .config/nvim/lua/cimino/packer.lua
+++ .config/nvim/lua/cimino/packer.lua
@@ -13,20 +13,27 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- Buffers
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
-
-  use { "catppuccin/nvim", as = "catppuccin" }
-
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
+  use('nvim-lualine/lualine.nvim')
+
+  -- Colorscheme
+  use { "catppuccin/nvim", as = "catppuccin" }
+
+  -- Treesitter
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+
+  -- Git
   use('tpope/vim-fugitive')
 
+  -- LSP
   use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  requires = {
