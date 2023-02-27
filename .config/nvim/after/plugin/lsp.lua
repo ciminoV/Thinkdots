07cb2ccb7fe1b800c102527fcbcf52835f0abba2
ciminoV <ciminovincenzo1@gmail.com>
2023-02-27 17:29:29 +0100

    feat nvim: update cmp keymaps

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -6,11 +6,40 @@ end
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'sumneko_lua',
+  'lua_ls',
   'bashls',
-  'texlab',
   'clangd',
-  'gopls'
+})
+
+-- Fix Undefined global 'vim'
+lsp.configure('lua-language-server', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+  ['<Tab>'] = vim.NIL, -- Disable tab completion
+  ['<S-Tab>'] = vim.NIL,
+})
+
+lsp.setup_nvim_cmp({
+  -- Don't preselect first match
+  preselect = 'none',
+  completion = {
+    completeopt = 'menu,menuone,noinsert,noselect'
+  },
+  mapping = cmp_mappings
 })
 
 lsp.set_preferences({
