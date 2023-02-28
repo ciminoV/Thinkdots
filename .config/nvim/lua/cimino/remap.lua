ddd357a9e83f3f5d9752a23116e848537d5d7ce2
ciminoV <ciminovincenzo1@gmail.com>
2023-02-28 18:01:44 +0100

    feat nvim: add vimtex plugin

--- .config/nvim/lua/cimino/remap.lua
+++ .config/nvim/lua/cimino/remap.lua
@@ -44,3 +44,8 @@ keymap("n", "mv", ":! opout %<CR><CR>")
 -- Nice remaps
 keymap("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 keymap("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- VimTex
+keymap("n", "<leader>vv", ":VimtexView<CR>")
+keymap("n", "<leader>vc", ":VimtexCompile<CR>")
+keymap("i", "<C-l>", "<C-g>u<Esc>[s1z=`]a<C-g>u") -- spell check on the fly
