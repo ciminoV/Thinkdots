f909c349b8e75841c1d554029fa05651965ea014
ciminoV <ciminovincenzo1@gmail.com>
2024-05-21 13:54:35 +0200

    nvim_feat: require root privileges simplesync

--- .config/nvim/lua/cimino/simplesync.lua
+++ .config/nvim/lua/cimino/simplesync.lua
@@ -1,10 +1,10 @@
 local script_path = vim.fn.expand("$HOME") .. "/documents/simplesync/"
 
-vim.api.nvim_create_user_command("NoteSync", "!python3 " .. script_path .. "sync.py", {})
+vim.api.nvim_create_user_command("NoteSync", "!sudo python3 " .. script_path .. "sync.py", {})
 
-vim.api.nvim_create_user_command("NoteUpdate", "!python3 " .. script_path .. "update.py <q-args>", { nargs = 1 })
+vim.api.nvim_create_user_command("NoteUpdate", "!sudo python3 " .. script_path .. "update.py <q-args>", { nargs = 1 })
 
-vim.api.nvim_create_user_command("NoteRemove", "!python3 " .. script_path .. "remove.py <q-args>", { nargs = 1 })
+vim.api.nvim_create_user_command("NoteRemove", "!sudo python3 " .. script_path .. "remove.py <q-args>", { nargs = 1 })
 
 local simple_augroup = vim.api.nvim_create_augroup("simplesync", { clear = false })
 local note_path = vim.fn.expand("$HOME") .. "/documents/notes/"
