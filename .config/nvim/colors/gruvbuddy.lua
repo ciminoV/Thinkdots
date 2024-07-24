3edec6a2a5dbac86ba81a8ea7e8643b46ebdcad4
ciminoV <ciminovincenzo1@gmail.com>
2024-07-24 12:17:53 +0200

    nvim feat: more colorscheme updates

--- .config/nvim/colors/gruvbuddy.lua
+++ .config/nvim/colors/gruvbuddy.lua
@@ -4,14 +4,14 @@ local colorbuddy = require("colorbuddy")
 local Color = colorbuddy.Color
 local Group = colorbuddy.Group
 local c = colorbuddy.colors
---local g = colorbuddy.groups
+local g = colorbuddy.groups
 local s = colorbuddy.styles
 
 Color.new("white", "#f2e5bc")
 Color.new("red", "#cc6666")
 Color.new("pink", "#fef601")
 Color.new("green", "#99cc99")
-Color.new("yellow", "#e5c890")
+Color.new("yellow", "#f8fe7a")
 Color.new("blue", "#81a2be")
 Color.new("aqua", "#8ec07c")
 Color.new("cyan", "#8abeb7")
@@ -23,35 +23,54 @@ Color.new("brown", "#a3685a")
 Color.new("seagreen", "#698b69")
 Color.new("turquoise", "#698b69")
 
-local background_string = "#111111"
+local background_string = "#282c34"
 Color.new("background", background_string)
+Color.new("gray0", background_string)
 
 Group.new("Normal", c.superwhite, c.gray0)
+Group.new("NormalFloat", c.superwhite, c.gray0)
+Group.new("Comment", c.gray4:dark(), nil, s.none)
 Group.new("Conceal", c.superwhite, nil, nil)
-Group.new("Character", c.cyan)
+Group.new("LineNr", c.gray2, c.gray0)
 Group.new("PreProc", c.violet)
-Group.new("Comment", c.gray3:dark(), nil, s.none)
 Group.new("Statement", c.brown)
-Group.new("Special", c.purple:light(), nil, s.none)
+Group.new("StorageClass", c.yellow)
+Group.new("Visual", nil, c.blue:dark(0.1))
+Group.new("VisualLineMode", g.Visual, g.Visual)
+Group.new("VisualMode", g.Visual, g.Visual)
 
-Group.new("@variable", c.superwhite, nil)
-Group.new("@variable.builtin", c.purple:light(), nil, s.bold) -- built-in variable names (e.g. `this`)
+-- @variable ; various variable names
+Group.new("@variable", g.Normal)
+-- @variable.builtin ; built-in variable names (e.g. `this`)
+Group.new("@variable.builtin", c.purple:light(), nil, s.bold)
+-- @variable.parameter  ; parameters of a function
+-- @variable.member     ; object and struct fields
 
-Group.new("@constant", c.orange, nil, s.none)
-Group.new("@constant.macro", c.orange, nil, s.none)
+-- @constant          ; constant identifiers
+Group.new("@constant", c.orange, nil, s.bold)
+-- @constant.builtin  ; built-in constant values
+-- @constant.macro    ; constants defined by the preprocessor
 
-Group.new("@type", c.violet, nil, s.none)
-Group.new("@type.builtin", c.violet, nil, s.none)
-Group.new("@type.definition", c.violet, nil, s.bold) -- identifiers in type definitions (e.g. `typedef <type> <identifier>` in C)
-Group.new("@type.qualifier", c.violet, nil, s.none)
+-- @module            ; modules or namespaces
+Group.new("@module", c.white)
+-- @module.builtin    ; built-in modules or namespaces
 
-Group.new("@function", c.blue, nil, s.none)
-Group.new("@constructor", c.blue, nil, s.bold)
+-- @character              ; character literals
+Group.new("Character", c.cyan)
 
-Group.new("@module", c.superwhite, nil, s.none) -- modules or namespaces
+-- @type             ; type or class definitions and annotations
+Group.new("Type", c.blue, nil, s.none)
+-- @type.builtin     ; built-in types
+Group.new("@type.builtin", g.type, nil, s.none)
+-- @type.qualifier   ; type qualifiers (e.g. `const`)
+Group.new("@type.qualifier", g.type, nil, s.none)
+-- @type.definition  ; identifiers in type definitions (e.g. `typedef <type> <identifier>` in C)
+Group.new("@type.definition", g.type, nil, s.italic)
 
-Group.new("@keyword", c.yellow, nil, s.none)
-Group.new("@keyword.conditional", c.violet, nil, s.none) -- keywords related to conditionals (e.g. `if` / `else`)
-Group.new("@keyword.repeat", c.violet, nil, s.none) -- keywords related to loops (e.g. `for` / `while`)
+-- @function             ; function definitions
+Group.new("@function", c.cyan, nil, s.none)
 
-Group.new("@label", c.pink)
+Group.new("@keyword", c.violet) -- ; keywords not fitting into specific categories
+Group.new("@keyword.repeat", c.blue) -- ; keywords not fitting into specific categories
+-- @keyword.repeat            ; keywords related to loops (e.g. `for` / `while`)
+-- @keyword.conditional         ; keywords related to conditionals (e.g. `if` / `else`)
--- .config/nvim/lua/cimino/lazy/colors.lua
+++ .config/nvim/lua/cimino/lazy/colors.lua
@@ -1,54 +1,67 @@
+function ColorMyPencils(color)
+	color = color or "rose-pine-moon"
+	vim.cmd.colorscheme(color)
+
+	-- Configure highlights
+	vim.cmd.hi("Comment gui=none")
+end
+
 return {
-	-- {
-	-- 	"catppuccin/nvim",
-	-- 	name = "catppuccin",
-	-- 	priority = 1000, -- Make sure to load this before all the other start plugins.
-	-- 	init = function()
-	-- 		require("catppuccin").setup({
-	-- 			flavour = "frappe",
-	-- 			background = {
-	-- 				light = "latte",
-	-- 				dark = "frappe",
-	-- 			},
-	-- 			transparent_background = false,
-	-- 			term_colors = true,
-	-- 			dim_inactive = {
-	-- 				enabled = true,
-	-- 				shade = "dark",
-	-- 				percentage = 0.5,
-	-- 			},
-	-- 			styles = {
-	-- 				comments = {},
-	-- 				conditionals = {},
-	-- 				loops = {},
-	-- 				functions = {},
-	-- 				keywords = {},
-	-- 				strings = {},
-	-- 				variables = {},
-	-- 				numbers = {},
-	-- 				booleans = {},
-	-- 				properties = {},
-	-- 				types = {},
-	-- 				operators = {},
-	-- 			},
-	-- 			integrations = {
-	-- 				cmp = true,
-	-- 				telescope = true,
-	-- 			},
-	-- 		})
-	-- 		vim.cmd.colorscheme("catppuccin")
-	--
-	-- 		-- You can configure highlights by doing something like:
-	-- 		vim.cmd.hi("Comment gui=none")
-	-- 	end,
-	-- },
 	{
 		"tjdevries/colorbuddy.nvim",
 		config = function()
-			vim.cmd.colorscheme("gruvbuddy")
-
-			-- Configure highlits
-			vim.cmd.hi("Comment gui=none")
+			ColorMyPencils("gruvbuddy")
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		init = function()
+			require("catppuccin").setup({
+				flavour = "frappe",
+				background = {
+					light = "latte",
+					dark = "frappe",
+				},
+				transparent_background = false,
+				term_colors = true,
+				dim_inactive = {
+					enabled = true,
+					shade = "dark",
+					percentage = 0.5,
+				},
+				styles = {
+					comments = {},
+					conditionals = {},
+					loops = {},
+					functions = {},
+					keywords = {},
+					strings = {},
+					variables = {},
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = {},
+					operators = {},
+				},
+				integrations = {
+					cmp = true,
+					telescope = true,
+				},
+			})
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true,
+				styles = {
+					italic = false,
+				},
+			})
 		end,
 	},
 }
