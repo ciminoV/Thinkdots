bbadb498945992cd8aac95bfcd982804c09c42f7
ciminoV <ciminovincenzo1@gmail.com>
2025-04-05 20:01:39 +0200

    nvim feat: fix cpp tabs

--- .config/nvim/lua/cimino/set.lua
+++ .config/nvim/lua/cimino/set.lua
@@ -18,7 +18,7 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 
 vim.opt.smartcase = true
 vim.opt.smartindent = true
